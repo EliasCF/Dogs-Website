bbb3343531ebe6413da2af64effec32cd3cd5b10
EliasCF <edimbo4@gmail.com>
2018-11-28 21:13:07 +0100

    Dog pictures are now structured in rows of 3

--- public/IndexApp.js
+++ public/IndexApp.js
@@ -13,7 +13,16 @@ var IndexApp = new Vue({
     methods: {
         getBreed: function (breed) {
             $.get(`api/dogs/breeds.php?breed=${breed}&amount=${this.amount}`, (data) => {
-                this.images = data;
+                this.images = [];
+
+                //Sort data into rows of 3
+                dogArray = [];
+                for (i = 0; i < data.length; i += 3) {
+                    dogArray.push(data.slice(i, i+3));
+                }
+
+                this.images = dogArray;
+                document.title = breed;
             });
         }
     }
