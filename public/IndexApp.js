462c0ce4a717797c4582abfa184724a6d6b015ac
EliasCF <edimbo4@gmail.com>
2018-11-29 16:21:19 +0100

    Added load button, that wil load 20 images at a time

--- public/IndexApp.js
+++ public/IndexApp.js
@@ -3,27 +3,52 @@ var IndexApp = new Vue({
     data: {
         dogs: {},
         images: {},
-        amount: 20
+        amount: 20,
+        currentBreed: ''
     },
     created: function () {
+        //Load a list of dog breeds
         $.get('api/dogs/breeds.php', (data) => {
             this.dogs = data;
         });
     },
     methods: {
+        /*
+         * @Description:
+         *      Loads an x amount of a certain breed from the api wrapper
+         * 
+         * @Params:
+         *      breed (string): 
+         *          The breed to be fetched
+         */
         getBreed: function (breed) {
+            //Reset this.amount in case the 'breed' variable is a new breed
+            if (breed != this.currentBreed) {
+                this.amount = 20;
+            }
+
             $.get(`api/dogs/breeds.php?breed=${breed}&amount=${this.amount}`, (data) => {
                 this.images = [];
 
                 //Sort data into rows of 3
                 dogArray = [];
                 for (i = 0; i < data.length; i += 3) {
-                    dogArray.push(data.slice(i, i+3));
+                    dogArray.push(data.slice(i, i + 3));
                 }
 
-                this.images = dogArray;
                 document.title = breed;
+                this.images = dogArray;
+                this.currentBreed = breed;
             });
+        },
+        /*
+         * @Description:
+         *      Loads an additional 20 images
+         */
+        loadMore: function() {
+            this.amount += 20;
+
+            this.getBreed(this.currentBreed);
         }
     }
 });
